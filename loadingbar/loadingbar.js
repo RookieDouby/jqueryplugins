5c255d12acf607aec65d09eef8fad6a7fbf78c4c
szhang <zhd1109@qq.com>
2017-01-05 16:40:52 +0800

    add loadingbar

---
+++ loadingbar/loadingbar.js
@@ -0,0 +1,10 @@
+(function($) {
+    var $box = $("#box");
+    var $bg = $("#bg");
+    var $bgcolor = $("#bgcolor");
+    var $btn = $("#btn");
+    var $process = $("#process"):
+    var status = false;
+    var  = 0;
+    var
+})(jQuery);
