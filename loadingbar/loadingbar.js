adfce9679af5f73c2e95b7d05c41caf8ff2f2229
sam zhang <szhang@movoto.com>
2017-01-09 21:51:11 +0800

    dynamic rectangle

--- loadingbar/loadingbar.js
+++ loadingbar/loadingbar.js
@@ -3,8 +3,52 @@
     var $bg = $("#bg");
     var $bgcolor = $("#bgcolor");
     var $btn = $("#btn");
-    var $process = $("#process"):
+    var $process = $("#process");
     var status = false;
-    var  = 0;
-    var
+    var ox = 0;
+    var left = 0;
+    var bgleft = 0;
+    
+    var setBtnPosition = function (left) {
+        $btn.css('left',left);
+        $bgcolor.width(left);
+        $process.html('Rrocess: ' + parseFloat(left/2).toFixed(2) + '%');
+    };
+    
+    $btn.mousedown(function (e) {
+        ox = e.pageX - left;
+        status = true;
+    });
+
+    $(document).mouseup(function (e) {
+        status = false;
+    });
+
+    $box.mousemove(function (e) {
+        if(status) {
+            left = e.pageX - ox;
+            if(left < 0) {
+                left = 0;
+            }
+            if(left > 200) {
+                left = 200;
+            }
+            setBtnPosition(left);
+        }
+    });
+    
+    $bg.click(function (e) {
+        if(!status) {
+            bgleft = $bg.offset().left;
+            left = e.pageX - bgleft;
+            if(left < 0) {
+                left = 0;
+            }
+            if(left > 200) {
+                left = 200;
+            }
+            setBtnPosition(left);
+        }
+    })
+
 })(jQuery);
